e8eec06d4c1cf5621ccabb80c614cf0d942e44c0
Gopika K T <gopikakt9041@gmail.com>
2025-12-09 16:00:17 +0530

    Create swap_two_numbers.py

---
+++ swap_two_numbers.py
@@ -0,0 +1,15 @@
+# Program to swap two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swapping logic
+a, b = b, a
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
